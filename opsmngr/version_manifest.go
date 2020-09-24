55ea3ef37abdfa35a35d9f5be83ec95a5d929693
andreaangiolillo <andrea.angiolillo@mongodb.com>
2020-09-24 19:39:08 +0200

    CLOUDP-73628: Add Version manifest to the opsmanager go client

---
+++ opsmngr/version_manifest.go
@@ -0,0 +1,105 @@
+// Copyright 2019 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package opsmngr
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	atlas "go.mongodb.org/atlas/mongodbatlas"
+)
+
+const (
+	versionManifestBasePath   = "versionManifest"
+	versionManifestStaticPath = "https://opsmanager.mongodb.com/static/version_manifest/%s"
+)
+
+// VersionManifestService is an interface for interfacing with the Version Manifest
+// endpoints of the MongoDB Ops Manager API.
+//
+// See more: https://docs.opsmanager.mongodb.com/current/reference/api/version-manifest/
+type VersionManifestService interface {
+	Get(context.Context, string) (*VersionManifest, *atlas.Response, error)
+	Update(context.Context, *VersionManifest) (*VersionManifest, *atlas.Response, error)
+}
+
+// VersionManifestServiceOp provides an implementation of the VersionManifestService interface
+type VersionManifestServiceOp service
+
+var _ VersionManifestService = &VersionManifestServiceOp{}
+
+type VersionManifest struct {
+	Updated  int64      `json:"updated,omitempty"`
+	Versions []*Version `json:"versions,omitempty"`
+}
+
+type Version struct {
+	Name   string   `json:"name,omitempty"`
+	Builds []*Build `json:"builds,omitempty"`
+}
+
+type Build struct {
+	Architecture       string    `json:"architecture,omitempty"`
+	GitVersion         string    `json:"gitVersion,omitempty"`
+	Platform           string    `json:"platform,omitempty"`
+	URL                string    `json:"url,omitempty"`
+	MaxOSVersion       string    `json:"maxOsVersion,omitempty"`
+	MinOSVersion       string    `json:"minOsVersion,omitempty"`
+	Win2008plus        bool      `json:"win2008plus,omitempty"`
+	WinVCRedistDLL     string    `json:"winVCRedistDll,omitempty"`
+	WinVCRedistOptions []*string `json:"winVCRedistOptions,omitempty"`
+	WinVCRedistURL     string    `json:"winVCRedistUrl,omitempty"`
+	WinVCRedistVersion string    `json:"winVCRedistVersion,omitempty"`
+	Flavor             string    `json:"flavor,omitempty"`
+}
+
+// Get gets the version manifest
+//
+// See more: https://docs.opsmanager.mongodb.com/current/reference/api/version-manifest/view-version-manifest/
+func (s *VersionManifestServiceOp) Get(ctx context.Context, version string) (*VersionManifest, *atlas.Response, error) {
+	if version == "" {
+		return nil, nil, atlas.NewArgError("version", "must be set")
+	}
+
+	path := fmt.Sprintf(versionManifestStaticPath, version)
+	req, err := http.NewRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	root := new(VersionManifest)
+	resp, err := s.Client.Do(ctx, req, root)
+	return root, resp, err
+}
+
+// Update updates the version manifest
+//
+// See more: https://docs.opsmanager.mongodb.com/current/reference/api/version-manifest/update-version-manifest/
+func (s *VersionManifestServiceOp) Update(ctx context.Context, versionManifest *VersionManifest) (*VersionManifest, *atlas.Response, error) {
+	if versionManifest == nil {
+		return nil, nil, atlas.NewArgError("versionManifest", "must be set")
+	}
+
+	req, err := s.Client.NewRequest(ctx, http.MethodPut, versionManifestBasePath, versionManifest)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	root := new(VersionManifest)
+	resp, err := s.Client.Do(ctx, req, root)
+
+	return root, resp, err
+}
---
+++ opsmngr/version_manifest_test.go
@@ -0,0 +1,175 @@
+// Copyright 2020 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package opsmngr
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/go-test/deep"
+)
+
+func TestVersionManifest_Get(t *testing.T) {
+	client, mux, teardown := setup()
+	defer teardown()
+	const version = "test.json"
+
+	mux.HandleFunc(fmt.Sprintf("https://opsmanager.mongodb.com/static/version_manifest/%s", version), func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		_, _ = fmt.Fprint(w, `
+
+		{
+		  "updated": 1599868800044,
+		  "versions": [
+			{
+			  "builds": [
+				{
+				  "architecture": "amd64",
+				  "gitVersion": "1c1c76aeca21c5983dc178920f5052c298db616c",
+				  "platform": "linux",
+				  "url": "/linux/mongodb-linux-x86_64-2.6.0.tgz"
+				},
+				{
+				  "architecture": "amd64",
+				  "gitVersion": "1c1c76aeca21c5983dc178920f5052c298db616c",
+				  "platform": "osx",
+				  "url": "/osx/mongodb-osx-x86_64-2.6.0.tgz"
+				}
+			  ],
+			  "name": "2.6.0"
+			}]
+		}`)
+	})
+
+	manifest, _, err := client.VersionManifest.Get(ctx, version)
+	if err != nil {
+		t.Fatalf("VersionManifest.Get returned error: %v", err)
+	}
+
+	expected := &VersionManifest{
+		Updated: 1599868800044,
+		Versions: []*Version{
+			{
+				Name: "2.6.0",
+				Builds: []*Build{
+					{
+						Architecture: "amd64",
+						GitVersion:   "1c1c76aeca21c5983dc178920f5052c298db616c",
+						Platform:     "linux",
+						URL:          "/linux/mongodb-linux-x86_64-2.6.0.tgz",
+					},
+					{
+						Architecture: "amd64",
+						GitVersion:   "1c1c76aeca21c5983dc178920f5052c298db616c",
+						Platform:     "osx",
+						URL:          "/osx/mongodb-osx-x86_64-2.6.0.tgz",
+					},
+				},
+			},
+		},
+	}
+
+	if diff := deep.Equal(manifest, expected); diff != nil {
+		t.Error(diff)
+	}
+}
+
+func TestVersionManifest_Update(t *testing.T) {
+	client, mux, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/versionManifest", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPut)
+		_, _ = fmt.Fprint(w, `
+
+		{
+		  "updated": 1599868800044,
+		  "versions": [
+			{
+			  "builds": [
+				{
+				  "architecture": "amd64",
+				  "gitVersion": "1c1c76aeca21c5983dc178920f5052c298db616c",
+				  "platform": "linux",
+				  "url": "/linux/mongodb-linux-x86_64-2.6.0.tgz"
+				},
+				{
+				  "architecture": "amd64",
+				  "gitVersion": "1c1c76aeca21c5983dc178920f5052c298db616c",
+				  "platform": "osx",
+				  "url": "/osx/mongodb-osx-x86_64-2.6.0.tgz"
+				}
+			  ],
+			  "name": "2.6.0"
+			}]
+		}`)
+	})
+
+	newManifest := &VersionManifest{
+		Updated: 1599868800044,
+		Versions: []*Version{
+			{
+				Name: "2.6.0",
+				Builds: []*Build{
+					{
+						Architecture: "amd64",
+						GitVersion:   "1c1c76aeca21c5983dc178920f5052c298db616c",
+						Platform:     "linux",
+						URL:          "/linux/mongodb-linux-x86_64-2.6.0.tgz",
+					},
+					{
+						Architecture: "amd64",
+						GitVersion:   "1c1c76aeca21c5983dc178920f5052c298db616c",
+						Platform:     "osx",
+						URL:          "/osx/mongodb-osx-x86_64-2.6.0.tgz",
+					},
+				},
+			},
+		},
+	}
+
+	newManifest, _, err := client.VersionManifest.Update(ctx, newManifest)
+	if err != nil {
+		t.Fatalf("VersionManifest.Update returned error: %v", err)
+	}
+
+	expected := &VersionManifest{
+		Updated: 1599868800044,
+		Versions: []*Version{
+			{
+				Name: "2.6.0",
+				Builds: []*Build{
+					{
+						Architecture: "amd64",
+						GitVersion:   "1c1c76aeca21c5983dc178920f5052c298db616c",
+						Platform:     "linux",
+						URL:          "/linux/mongodb-linux-x86_64-2.6.0.tgz",
+					},
+					{
+						Architecture: "amd64",
+						GitVersion:   "1c1c76aeca21c5983dc178920f5052c298db616c",
+						Platform:     "osx",
+						URL:          "/osx/mongodb-osx-x86_64-2.6.0.tgz",
+					},
+				},
+			},
+		},
+	}
+
+	if diff := deep.Equal(newManifest, expected); diff != nil {
+		t.Error(diff)
+	}
+}
