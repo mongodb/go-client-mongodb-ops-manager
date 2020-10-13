52c09bdb177292928ccb7b984b0b590f0deeb082
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2020-10-13 17:26:49 +0100

    CLOUDP-74965: Implementing snapshot schedule api - ops manager go client (#63)

---
+++ opsmngr/snapshot_schedule.go
@@ -0,0 +1,109 @@
+// Copyright 2020 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package opsmngr
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	atlas "go.mongodb.org/atlas/mongodbatlas"
+)
+
+const snapshotScheduleBasePath = "groups/%s/backupConfigs/%s/snapshotSchedule"
+
+// SnapshotScheduleService is an interface for using the Snapshot schedule
+// endpoints of the MongoDB Ops Manager API.
+//
+// See more: https://docs.opsmanager.mongodb.com/current/reference/api/snapshot-schedule/
+type SnapshotScheduleService interface {
+	Get(context.Context, string, string) (*SnapshotSchedule, *atlas.Response, error)
+	Update(context.Context, string, string, *SnapshotSchedule) (*SnapshotSchedule, *atlas.Response, error)
+}
+
+// SnapshotScheduleServiceOp provides an implementation of the SnapshotScheduleService interface
+type SnapshotScheduleServiceOp service
+
+var _ SnapshotScheduleService = &SnapshotScheduleServiceOp{}
+
+type SnapshotSchedule struct {
+	ClusterID                      string        `json:"clusterId"`
+	GroupID                        string        `json:"groupId"`
+	ReferenceTimeZoneOffset        string        `json:"referenceTimeZoneOffset,omitempty"`
+	DailySnapshotRetentionDays     int           `json:"dailySnapshotRetentionDays,omitempty"`
+	ClusterCheckpointIntervalMin   int           `json:"clusterCheckpointIntervalMin,omitempty"`
+	Links                          []*atlas.Link `json:"links,omitempty"`
+	MonthlySnapshotRetentionMonths int           `json:"monthlySnapshotRetentionMonths,omitempty"`
+	PointInTimeWindowHours         *int          `json:"pointInTimeWindowHours,omitempty"`
+	ReferenceHourOfDay             *int          `json:"referenceHourOfDay,omitempty"`
+	ReferenceMinuteOfHour          *int          `json:"referenceMinuteOfHour,omitempty"`
+	SnapshotIntervalHours          int           `json:"snapshotIntervalHours,omitempty"`
+	SnapshotRetentionDays          int           `json:"snapshotRetentionDays,omitempty"`
+	WeeklySnapshotRetentionWeeks   int           `json:"weeklySnapshotRetentionWeeks,omitempty"`
+}
+
+// Get gets the snapshot schedule for an instance.
+//
+// See more: https://docs.opsmanager.mongodb.com/current/reference/api/backup/get-snapshot-schedule/
+func (s *SnapshotScheduleServiceOp) Get(ctx context.Context, groupID, clusterID string) (*SnapshotSchedule, *atlas.Response, error) {
+	if groupID == "" {
+		return nil, nil, atlas.NewArgError("groupID", "must be set")
+	}
+
+	if clusterID == "" {
+		return nil, nil, atlas.NewArgError("clusterID", "must be set")
+	}
+
+	path := fmt.Sprintf(snapshotScheduleBasePath, groupID, clusterID)
+	req, err := s.Client.NewRequest(ctx, http.MethodGet, path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	root := new(SnapshotSchedule)
+	resp, err := s.Client.Do(ctx, req, root)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return root, resp, err
+}
+
+// Update updates the parameters of snapshot creation and retention.
+//
+// See more: https://docs.opsmanager.mongodb.com/current/reference/api/backup/update-one-snapshot-schedule-by-cluster-id/
+func (s *SnapshotScheduleServiceOp) Update(ctx context.Context, groupID, clusterID string, snapshot *SnapshotSchedule) (*SnapshotSchedule, *atlas.Response, error) {
+	if groupID == "" {
+		return nil, nil, atlas.NewArgError("groupID", "must be set")
+	}
+
+	if clusterID == "" {
+		return nil, nil, atlas.NewArgError("clusterID", "must be set")
+	}
+
+	path := fmt.Sprintf(snapshotScheduleBasePath, groupID, clusterID)
+	req, err := s.Client.NewRequest(ctx, http.MethodPatch, path, snapshot)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	root := new(SnapshotSchedule)
+	resp, err := s.Client.Do(ctx, req, root)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return root, resp, err
+}
---
+++ opsmngr/snapshot_schedule_test.go
@@ -0,0 +1,114 @@
+// Copyright 2020 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package opsmngr
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/go-test/deep"
+)
+
+func TestSnapshotScheduleServiceOp_Get(t *testing.T) {
+	client, mux, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc(fmt.Sprintf("/groups/%s/backupConfigs/%s/snapshotSchedule", groupID, clusterID), func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		_, _ = fmt.Fprint(w, `{
+			  "clusterId" : "6b8cd61180eef547110159d9",
+			  "dailySnapshotRetentionDays" : 7,
+			  "groupId" : "5c8100bcf2a30b12ff88258f",
+			  "monthlySnapshotRetentionMonths" : 13,
+			  "pointInTimeWindowHours": 24,
+			  "snapshotIntervalHours" : 6,
+			  "snapshotRetentionDays" : 2,
+			  "weeklySnapshotRetentionWeeks" : 4
+}`)
+	})
+
+	snapshot, _, err := client.SnapshotSchedule.Get(ctx, groupID, clusterID)
+	if err != nil {
+		t.Fatalf("SnapshotSchedule.Get returned error: %v", err)
+	}
+
+	pointInTimeWindowHours := 24
+
+	expected := &SnapshotSchedule{
+		ClusterID:                      clusterID,
+		GroupID:                        groupID,
+		MonthlySnapshotRetentionMonths: 13,
+		PointInTimeWindowHours:         &pointInTimeWindowHours,
+		SnapshotIntervalHours:          6,
+		SnapshotRetentionDays:          2,
+		WeeklySnapshotRetentionWeeks:   4,
+		DailySnapshotRetentionDays:     7,
+	}
+	if diff := deep.Equal(snapshot, expected); diff != nil {
+		t.Error(diff)
+	}
+}
+
+func TestSnapshotScheduleServiceOp_Update(t *testing.T) {
+	client, mux, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc(fmt.Sprintf("/groups/%s/backupConfigs/%s/snapshotSchedule", groupID, clusterID), func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPatch)
+		_, _ = fmt.Fprint(w, `{
+			  "clusterId" : "6b8cd61180eef547110159d9",
+			  "dailySnapshotRetentionDays" : 7,
+			  "groupId" : "5c8100bcf2a30b12ff88258f",
+			  "monthlySnapshotRetentionMonths" : 13,
+			  "pointInTimeWindowHours": 24,
+			  "snapshotIntervalHours" : 6,
+			  "snapshotRetentionDays" : 2,
+			  "weeklySnapshotRetentionWeeks" : 4
+}`)
+	})
+
+	pointInTimeWindowHours := 24
+
+	snapshotSchedule := &SnapshotSchedule{
+		ClusterID:                      clusterID,
+		GroupID:                        groupID,
+		MonthlySnapshotRetentionMonths: 13,
+		PointInTimeWindowHours:         &pointInTimeWindowHours,
+		SnapshotIntervalHours:          6,
+		SnapshotRetentionDays:          2,
+		WeeklySnapshotRetentionWeeks:   4,
+		DailySnapshotRetentionDays:     7,
+	}
+
+	snapshot, _, err := client.SnapshotSchedule.Update(ctx, snapshotSchedule.GroupID, snapshotSchedule.ClusterID, snapshotSchedule)
+	if err != nil {
+		t.Fatalf("SnapshotSchedule.Update returned error: %v", err)
+	}
+
+	expected := &SnapshotSchedule{
+		ClusterID:                      clusterID,
+		GroupID:                        groupID,
+		MonthlySnapshotRetentionMonths: 13,
+		PointInTimeWindowHours:         &pointInTimeWindowHours,
+		SnapshotIntervalHours:          6,
+		SnapshotRetentionDays:          2,
+		WeeklySnapshotRetentionWeeks:   4,
+		DailySnapshotRetentionDays:     7,
+	}
+	if diff := deep.Equal(snapshot, expected); diff != nil {
+		t.Error(diff)
+	}
+}
